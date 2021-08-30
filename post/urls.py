f1829f4fdb8897ce313911923b372bb8d24a4711
Tuba Adıgüzel <71885903+tubaAdgzl@users.noreply.github.com>
2021-08-30 19:34:29 +0300

    urls updated

--- post/urls.py
+++ post/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . views import *
 
+# app_name="blog"
+
 urlpatterns = [
     path('',post_index,name="index"),
     path('post/create',PostCreateView.as_view(),name="create"),
     path('post/detail/<slug:slug>',post_detail,name="detail"),
     path('post/update/<slug:slug>',PostUpdateView.as_view(),name="update"),
     path('post/delete/<slug:slug>',PostDeleteView.as_view(),name="delete"),
+    path('like/<slug:slug>',post_like, name="like_post"),
 ]
